e8c1b979ae0e5055bb7a96a948ca6e6ff4248664
gmlyytt-YANG <gmlyytt@outlook.com>
2019-04-26 16:02:34 +0800

    add leetcode 14

---
+++ leetcode/leetcode_14. Longest Common Prefix.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+ *
+ * Copyright (c) 2018 Administrator. All Rights Reserved
+ *
+ ************************************************************************/
+
+/*
+ * @file leetcode_14. Longest Common Prefix.h
+ * @author gmlyytt@outlook.com
+ * @date 2019/04/26 15:52:00
+ * @brief https://leetcode.com/problems/longest-common-prefix/
+ * */
+
+#ifndef INC_1STPROGRAM_LEETCODE_14_LONGEST_COMMON_PREFIX_H
+#define INC_1STPROGRAM_LEETCODE_14_LONGEST_COMMON_PREFIX_H
+
+#endif //INC_1STPROGRAM_LEETCODE_14_LONGEST_COMMON_PREFIX_H
